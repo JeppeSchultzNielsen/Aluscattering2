aac665b9fb2189e7be74a8112693b8849b737d84
Jeppe Schultz Nielsen <jeppe-schultz@hotmail.com>
2022-03-07 19:06:28 +0100

    Læse for mul

--- Vinkelhistogrammer.cpp
+++ Vinkelhistogrammer.cpp
@@ -17,6 +17,7 @@
 #include <ctime>
 #include <libconfig.h++>
 #include "include/Hit.h"
+#include "TH1.h"
 
 using namespace std;
 using namespace AUSA;
@@ -35,24 +36,25 @@ int main(int argc, char *argv[]){
 
     //Skab variable som værdier kan loades ned i. Associer dem med branches i træet, så den i. entry bliver lagt
     //deri når vi kalder getEntry(i). Print antallet af entries i træet.
-    double E[10];
-    double scatterAngle[10];
+    double E[100];
+    double scatterAngle[100];
+    UInt_t mul;
     t->SetBranchAddress("E",&E);
     t->SetBranchAddress("scatterAngle",&scatterAngle);
+    t->SetBranchAddress("mul",&mul);
     auto entries = t->GetEntries();
     cout << "Entries: " << entries << endl;
 
     //Lav et histogram
     TH1* h1 = new TH1I("h1", "Histogram ved scatteringangle 80 grader", 2000, 0.0, 12000);
-    cout << "Entries: " << entries << endl;
     for (Int_t i=0; i<entries; i++){
         t->GetEntry(i);
-        cout << "I " << i << endl;
-        //problem: Hvad hvis der er flere hits indeni dette leaf?
-        if(scatterAngle[0]>80 && scatterAngle[0]<81){
-            h1->Fill(E[0]);
+        for(Int_t j=0; j<mul; j++) {
+            if (scatterAngle[j] > 80 && scatterAngle[j] < 81) {
+                h1->Fill(E[j]);
+            } else {};
         }
-        else{};
     }
-    h1->Draw();
+    //Fit 4 Gausser til det.
+    TF1  *f1 = new TF1("f1","sin(x)/x",0,10);
 }
