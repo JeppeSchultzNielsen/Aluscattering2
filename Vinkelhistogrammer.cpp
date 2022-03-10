aab65f63184dc4e9b706ea62ee03f98759178ff9
jeppe <201908734@post.au.dk>
2022-03-10 12:58:15 +0100

    Fitter virker, men ca. 20 histogrammer har ikke konvergens.

--- Vinkelhistogrammer.cpp
+++ Vinkelhistogrammer.cpp
@@ -24,6 +24,7 @@
 #include <fstream>
 #include "TVirtualFitter.h"
 #include "TCanvas.h"
+#include "TFitResult.h"
 
 using namespace std;
 using namespace AUSA;
@@ -36,8 +37,7 @@ using namespace ROOT;
 
 double gaussSum(double *x, double *par){
     int npeaks = par[0];
-    npeaks = 1;
-    double_t result;
+    double_t result = 0;
     for (Int_t p=0;p<npeaks;p++) {
         Double_t norm = par[3 * p+1]; // "height" or "area"
         Double_t mean = par[3 * p + 2];
@@ -49,7 +49,7 @@ double gaussSum(double *x, double *par){
 
 bool AreSame(double a, double b)
 {
-    return fabs(a - b) < 0.1;
+    return fabs(a - b) < 0.5;
 }
 
 //løber igennem listen af angles og returnerer true hvis vinklen er i listen
@@ -128,58 +128,60 @@ int main(int argc, char *argv[]){
     }
     cout << "Lastangle: " << lastPrinted << endl;
     cout << "Lastangle: " << angles[7] << endl;
+    int nconverged = 0;
+    int ntot = 0;
     //nu skal jeg finde peaks i alle histogrammerne. Jeg looper over histogrammerne:
-    for(int i = 0; i < lastPrinted; i++){
+    for(int i = 0; i < lastPrinted; i++) {
         //Find peaks med inspiration fra https://root.cern/doc/master/peaks_8C.html
         //TSpectrum kan finde peaks
-        auto *s = new TSpectrum(100);
         TH1I *currentHist = histograms[i];
-        Int_t nfound = s->Search(currentHist, 2.5, "", 0.1);
 
-        auto xpeaks = s->GetPositionX();
+        //vi vil kun gøre det følgende, hvis vi har mere end 1000 entries i histogrammet, ellers giver det ikke
+        //rigtigt mening.
+        if (currentHist->GetEntries() > 1000) {
+            ntot += 1;
+            auto *s = new TSpectrum(100);
+            Int_t nfound = s->Search(currentHist, 10, "", 0.1);
 
-        //For hvert peak skal jeg bruge 3 parametre til at bestemme en gauss. (og så skal jeg give en mere til
-        //funktionen for at fortælle den, hvor mange peaks der er, men den fixes til npeaks).
-        double par[nfound*3+1];
-        //gider ikke dem der har mange peaks
-        if(nfound < 8){
-            for (int p = 0; p < nfound; p++) {
-                Double_t xp = xpeaks[p];
-                Int_t bin = currentHist->GetXaxis()->FindBin(xp);
-                Double_t yp = currentHist->GetBinContent(bin);
-                par[1+3*p]=yp;
-                par[2+3*p]=xp;
-                par[3+3*p]=10;
-                myfile << to_string(angles[i]) + "\t" + to_string(xp) + "\n";
-            }
-            TF1 *fit = new TF1("fit", gaussSum,0,2000, 1+3*nfound);
-            //TVirtualFitter tillader vist at vi kan have flere parametre?
-            //TVirtualFitter::Fitter(currentHist,10+3*nfound);
-            fit->SetParameters(par);
-            fit->FixParameter(0,nfound);
-            fit->SetNpx(2000);
-            currentHist->Fit("fit");
-            for (int k = 0; k < nfound; k++){
-                myfile << to_string(angles[i]) + "\t" + to_string(fit->GetParameter(2+3*k)) + "\n";
-            }
-            if(i == 50){
-                auto can = new TCanvas("t","t",800,800);
-                TFile output("file.root", "RECREATE");
-                //can->cd();
-                //currentHist->Draw();
-                //TF1 *func = new TF1("fit", gaussSum,1500,2000, 1+3*nfound);
-                //func->SetParameters(par);
-                //func->Draw("SAME");
-                //TF1 *func = new TF1("func", "[0]*exp(-0.5*(x-[1])*(x-[1])/[2]/[2])",0,2000);
-                //func->SetNpx(2000);
-                //func->FixParameter(0,par[4]);
-                //func->FixParameter(1,par[5]);
-                //func->FixParameter(2,par[6]);
-                //currentHist->Fit("func");
-                output.cd();
-                currentHist->Write();
+            auto xpeaks = s->GetPositionX();
+
+            //For hvert peak skal jeg bruge 3 parametre til at bestemme en gauss. (og så skal jeg give en mere til
+            //funktionen for at fortælle den, hvor mange peaks der er, men den fixes til npeaks).
+            double par[nfound * 3 + 1];
+            //gider ikke dem der har mange peaks
+            if (nfound < 8) {
+                for (int p = 0; p < nfound; p++) {
+                    Double_t xp = xpeaks[p];
+                    Int_t bin = currentHist->GetXaxis()->FindBin(xp);
+                    Double_t yp = currentHist->GetBinContent(bin);
+                    par[1 + 3 * p] = 3 * yp;
+                    par[2 + 3 * p] = xp;
+                    par[3 + 3 * p] = 20;
+                    myfile << to_string(angles[i]) + "\t" + to_string(xp) + "\n";
+                }
+                TF1 *fit = new TF1("fit", gaussSum, 0, 2000, 1 + 3 * nfound);
+                //TVirtualFitter tillader vist at vi kan have flere parametre?
+                //TVirtualFitter::Fitter(currentHist,10+3*nfound);
+                fit->SetParameters(par);
+                fit->FixParameter(0, nfound);
+                fit->SetNpx(2000);
+                TFitResultPtr fp = currentHist->Fit("fit","s && Q");
+                if (fp->IsValid()) {
+                    nconverged += 1;
+                    for (int k = 0; k < nfound; k++) {
+                        myfile << to_string(angles[i]) + "\t" + to_string(fit->GetParameter(2 + 3 * k)) + "\n";
+                    }
+                }
+                //vi kan lige gemme et histogram hvis det er
+                if (i == 148) {
+                    TFile output("file.root", "RECREATE");
+                    output.cd();
+                    currentHist->Write();
+                }
             }
         }
     }
+    cout << "Converged: " << nconverged << endl;
+    cout << "Total: " << ntot << endl;
     myfile.close();
 }
